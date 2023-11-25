cc81f94d675bef3ee5c07845cbf4fc6679a1710b
login <mkanderson771@gmail.com>
2023-11-26 01:50:43 +0300

    Using conditional statement and loops in php

--- grade.php
+++ grade.php
@@ -1,67 +1,62 @@
-<html>
-<HEAD>
- <title> using if.. elseif</title>
-</HEAD>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body Bgcolor="Teal">
-<h1>Grade checking</h1>
-<?php
-$mark=-7;
-echo "<br> <B> mark=$mark </B> </br>";
-if ($mark<0||$mark>100)
-{
-echo "invalid range <br>";
-}
+    <h1>Grade Checking</h1>
+    <?php
+    // if...else
+    $mark = 65;
+    if ($mark < 0 || $mark > 100){
+        echo "Out of Range";
+    }
+    else if($mark < 40){
+        echo "Grade E";
 
-elseif($mark<40)
-{
-echo "E";
-}
-
-elseif($mark<50)
-{echo "Grade D";
-}
-elseif($mark<60)
-{echo "Grade C ";
-}
-elseif($mark<70)
-{echo "Grade B";
-}
-else{
-echo "Grade A";
-}
-for($num=1;$num<20;$num++){
-if($num%2==0)
-{
-echo "our number is:$num<br>";
-}
-}
-$val=7;
-$factorial=1;
-$i=1;
-while($i<=7)
-{
-$factorial=$factorial*$i;
-$i++;
-}
-echo "<br>the factorial is$factorial </br>";
-$x=2;
-do {
-echo"our number is:$x<br>";
-$x+=2;
-}
-while($x<20);
-$Age=array(30,55,80,40,60);
-foreach($Age as $value)
-{
-echo"$value<br>";
-$sum=0;
-$sum= $sum + $$value;
-echo"$sum";
-}
-
- 
-
-?>
-
+    }
+    else if($mark < 50){
+        echo "Grade D";
+    }
+    else if($mark < 60){
+        echo "Grade C";
+    }
+    else if($mark < 70){
+        echo "Grade B";
+    }
+    else{
+        echo "Grade A";
+    }
+    echo "<p>
+    <B>mark = $mark</B>
+    <p>";
+    // for loops
+    for($num = 1; $num <= 20; $num++){
+        if($num % 2 == 0){
+        echo "Our number is: $num<BR>";
+        }
+    }
+    //while loops
+    $num = 19;
+    $k = $num;
+    $factorial = 1;
+    while($num != 0){
+        $factorial *= $num;
+        $num--;
+     }
+    echo "The factorial of  $k is: $factorial<BR>";
+     // Do....while
+    $k = 1;
+    do{
+        if($k % 2 == 0){
+            echo "The number is ".$k."<br>";
+        }
+        $k++;
+    }
+    while($k <= 20) 
+    ?>
 </body>
 </html>
+
